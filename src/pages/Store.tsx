15fdf813b1460dfb3e0dc04ba3bc8ee8222ee520
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-05-30 12:39:28 -0500

    added store items to page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,7 +1,15 @@
-
+import storeItems from "../data/items.json"
+import { Row, Col } from "react-bootstrap"
 const Store = () => {
   return (
-    <div>Store</div>
+    <>
+        <div>Store</div>
+        <Row>
+            {storeItems.map (item => (
+                <Col>{JSON.stringify(item)}</Col>
+            ))}
+        </Row>
+    </>
   )
 }
 
