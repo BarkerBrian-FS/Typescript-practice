530997e8b8aef93ab12680c882c589752b79c9f3
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-06-01 12:51:12 -0500

    working on cart pull out tab

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,10 @@
 import {Button, Container, Nav,  Navbar as Navbarbs} from "react-bootstrap"
 import { NavLink } from "react-router-dom"
 import { FaShoppingCart } from "react-icons/fa";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 
 const NavBar = () => {
+    const {openCart, cartQuantity} = useShoppingCart()
   return (
     <Navbarbs sticky="top" className="bg-white shadow-lg mb-3">
         <Container>
@@ -17,16 +19,17 @@ const NavBar = () => {
                     About
                 </Nav.Link>
             </Nav>
-            <Button style ={{width: "3rem", height: "3rem", position:"relative"}}
+            {cartQuantity > 0 &&( 
+            <Button onClick={openCart} style ={{width: "3rem", height: "3rem", position:"relative"}}
             variant="outline-primary" className="rounded-circle">
                 <FaShoppingCart size={24} />
                 <div className="rounded-circle bg-danger d-flex 
                 justify-content-center align-items-center" style={{color:"white", 
                     width:"1.5rem", height:"1.5rem", position:"absolute", 
                     bottom: 0, right: 0, transform:"translate(25%, 25%"}}>
-                    3
+                    {cartQuantity}
                 </div>
-            </Button>
+            </Button>)}
         </Container>
     </Navbarbs>
   )
--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,8 +1,25 @@
+import { Offcanvas, OffcanvasBody, OffcanvasHeader, OffcanvasTitle, Stack } from "react-bootstrap"
+import { useShoppingCart } from "../context/ShoppingCartContext"
+import { CartItem } from "./CartItem"
+type ShoppingCartProps ={
+    isOpen: boolean
+}
 
-
-const ShoppingCart = () => {
+const ShoppingCart = ({isOpen} : ShoppingCartProps) => {
+    const {closeCart, cartItems} = useShoppingCart()
   return (
-    <div>ShoppingCart</div>
+    <Offcanvas show={isOpen} onHide={closeCart} placement="end">
+        <OffcanvasHeader closeButton>
+            <OffcanvasTitle>Cart</OffcanvasTitle>
+        </OffcanvasHeader>
+        <OffcanvasBody>
+            <Stack gap={3}>
+                {cartItems.map(item => (
+                    <CartItem key={item.id} {...item} />
+                ))}
+            </Stack>
+        </OffcanvasBody>
+    </Offcanvas>
   )
 }
 
