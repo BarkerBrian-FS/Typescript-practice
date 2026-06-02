175c8d9aab1f3ac64425cf3414455483d025ecdd
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-06-02 10:39:22 -0500

    make local storage hook

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,6 +1,8 @@
 import { Offcanvas, OffcanvasBody, OffcanvasHeader, OffcanvasTitle, Stack } from "react-bootstrap"
 import { useShoppingCart } from "../context/ShoppingCartContext"
 import { CartItem } from "./CartItem"
+import formatCurrency from "../utilities/formatCurrency"
+import { StoreItem } from "./StoreItem"
 type ShoppingCartProps ={
     isOpen: boolean
 }
@@ -17,6 +19,12 @@ const ShoppingCart = ({isOpen} : ShoppingCartProps) => {
                 {cartItems.map(item => (
                     <CartItem key={item.id} {...item} />
                 ))}
+                <div className="ms-auto fw-bold fs-5">
+                    Total {formatCurrency(cartItems.reduce((total, cartItem)=>{
+                        const item = StoreItem.find(i => i.id === cartItem.id)
+                        return total + (item?.price || 0) * cartItem.quantity
+                    }, 0))}
+                </div>
             </Stack>
         </OffcanvasBody>
     </Offcanvas>
